a28d158a03234c0af33d6727dcc7319dcc82aede
Steve Tyler <mail@steve-tyler.co.uk>
2018-02-09 19:27:41 +0000

    created subscrbe component and added to home

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,12 +2,14 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import Jumbo from './Jumbo'
 import Payments from './Payments'
+import Subscribe from './Subscribe'
 
 const Home = () => {
   return (
     <div>
       <Jumbo />
       <Payments />
+      <Subscribe />
     </div>
   )
 }
---
+++ src/components/Subscribe.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import SubscribeForm from '../SubscribeForm'
+
+const formProps = {
+  action: 'https://foranimals.us17.list-manage.com/subscribe/post?u=8ce191ed37a0fc0aca5ecd011&amp;id=cdc577d0c9',
+  messages: {
+    inputPlaceholder: "First Name",
+    inputPlaceholder: "Email Address",
+    btnLabel: "Subscribe",
+    sending: "Please wait...",
+    success: "Please check your email to verify address",
+    error: "Error, please re-enter your email address"
+  },
+  styles: {
+    sending: {
+      fontSize: 18,
+      color: "auto"
+    },
+    success: {
+      fontSize: 18,
+      color: "green"
+    },
+    error: {
+      fontSize: 18,
+      color: "red"
+    }
+  }
+}
+
+export default () => <SubscribeForm {...formProps}/>
