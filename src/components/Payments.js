5b028968e18a98d48614c99cb73f9969587d634f
Steve Tyler <mail@steve-tyler.co.uk>
2018-02-10 17:54:11 +0000

    updated tsyling for header nav and payments

--- src/components/Payments.js
+++ src/components/Payments.js
@@ -8,25 +8,21 @@ const Payments = () =>
     <div class="container">
       <h2>Szybka pomoc</h2>
       <div className="row">
-        <div className="col-12 col-md-6">
-          <div className="row">
+        <div className="col col-md-6">
+          <div class="card">
             <i className="fab fa-cc-paypal fa-3x" />
-          </div>
-          <div class="row">
-						<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
-						<input type="hidden" name="cmd" value="_s-xclick" />
-						<input type="hidden" name="hosted_button_id" value="ER2HYC6HSTJWY" />
-						<input type="submit" formtarget="_blank" value="Przekaż darowiznę z PayPal" />
-						<img alt="" border="0" src="https://www.paypalobjects.com/pl_PL/i/scr/pixel.gif" width="1" height="1" />
-						</form>
+            <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+  						<input type="hidden" name="cmd" value="_s-xclick" />
+  						<input type="hidden" name="hosted_button_id" value="ER2HYC6HSTJWY" />
+  						<input type="submit" formtarget="_blank" value="Przekaż darowiznę z PayPal" class="btn btn-primary"/>
+  						<img alt="" border="0" src="https://www.paypalobjects.com/pl_PL/i/scr/pixel.gif" width="1" height="1" />
+  					</form>
           </div>
         </div>
-        <div className="col-12 col-md-6">
-          <div className="row">
+        <div className="col col-md-6">
+          <div class="card">
             <img src={payu} className="img-fluid mx-auto my-auto" alt=""/>
-          </div>
-          <div className="row">
-						<PayU />
+  					<PayU />
           </div>
         </div>
       </div>
