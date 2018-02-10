0226176d8b9b54b13b74f1113c52c34148266164
Steve Tyler <mail@steve-tyler.co.uk>
2018-02-10 22:22:52 +0000

    restyled payment buttons

--- src/components/Payments.js
+++ src/components/Payments.js
@@ -1,28 +1,30 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import payu from '../payu.svg'
 import PayU from './PayU'
 
 const Payments = () =>
   <section class="bg-primary text-center">
     <div class="container">
       <h2>Szybka pomoc</h2>
-      <div className="row">
-        <div className="col col-md-6">
-          <div class="card">
-            <i className="fab fa-cc-paypal fa-3x" />
+      <div className="row justify-content-center mb-5 mt-5">
+        <div className="col-md-12 col-lg-6">
+          <div class="">
             <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
-  						<input type="hidden" name="cmd" value="_s-xclick" />
-  						<input type="hidden" name="hosted_button_id" value="ER2HYC6HSTJWY" />
-  						<input type="submit" formtarget="_blank" value="Przekaż darowiznę z PayPal" class="btn btn-primary"/>
-  						<img alt="" border="0" src="https://www.paypalobjects.com/pl_PL/i/scr/pixel.gif" width="1" height="1" />
-  					</form>
+              <input type="hidden" name="cmd" value="_s-xclick" />
+              <input type="hidden" name="hosted_button_id" value="ER2HYC6HSTJWY" />
+
+              <button type="submit" formtarget="_blank" class="btn btn-primary btn-xl btn-donate">
+                Przekaż darowiznę z
+                <i className="fab fa-cc-paypal fa-3x" />
+              </button>
+              <img alt="" border="0" src="https://www.paypalobjects.com/pl_PL/i/scr/pixel.gif" />
+            </form>
           </div>
+
         </div>
-        <div className="col col-md-6">
-          <div class="card">
-            <img src={payu} className="img-fluid mx-auto my-auto" alt=""/>
-  					<PayU />
+        <div className="col-md-12 col-lg-6">
+          <div class="">
+            <PayU />
           </div>
         </div>
       </div>
