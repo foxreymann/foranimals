770d9f549c7115e01c93e87f0586fa4852a308a8
Steve Tyler <stevetyler.uk@gmail.com>
2018-02-11 23:30:11 +0000

    added contact details to home

---
+++ src/components/Contact.js
@@ -0,0 +1,46 @@
+import React from 'react';
+
+export default () =>
+  <section class="text-center">
+    <div class="container p-5">
+      <div className="row">
+        <div class="col-12 col-md-6">
+          <h2 class="pb-3">Dane Fundacji:</h2>
+          <ul class="list-unstyled">
+            <li>Adres:</li>
+            <li>Fundacja For Animals</li>
+            <li>ul. 11 Listopada 4</li>
+            <li>40-384 Katowice</li>
+            <li>KRS: 0000265307</li>
+          </ul>
+          <ul class="list-unstyled">
+            <li>Konto bankowe:</li>
+            <li>Bank PKO BP, konto nr:</li>
+            <li>02 1020 2313 0000 3102 0161 2043</li>
+            <li>REGULAMIN DAROWIZN</li>
+          </ul>
+        </div>
+        <div class="col-12 col-md-6">
+          <h2 class="pb-3">Ważne kontakty:</h2>
+
+          <h3>Katowice:</h3>
+          <ul class="list-unstyled">
+            <li>e-mail: pini1@foranimals.org.pl</li>
+            <li>tel.: 668-400-510</li>
+          </ul>
+
+          <h3>Łódź:</h3>
+          <ul class="list-unstyled">
+            <li>Anna (‘ansk’): </li>
+            <li>e-mail: ansk@foranimals.org.pl</li>
+            <li>http://www.foranimals-lodz.pl</li>
+          </ul>
+
+          <h3>Grudziądz:</h3>
+          <ul class="list-unstyled">
+            <li>e-mail: koty.grudziadz@poczta.onet.pl</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </section>
