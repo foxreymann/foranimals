e3b15ee716c39ab4f465b2cede3fee9ab051abe2
Steve Tyler <mail@steve-tyler.co.uk>
2018-02-11 16:06:37 +0000

    updated donate section styling

--- src/components/Payments.js
+++ src/components/Payments.js
@@ -6,24 +6,25 @@ const Payments = () =>
   <section class="bg-primary text-center">
     <div class="container">
       <h2>Szybka pomoc</h2>
-      <div className="row justify-content-center mb-5 mt-5">
+      <div className="row">
         <div className="col-md-12 col-lg-6">
-          <div class="">
+          <div class="paypal-wrap">
+            <div class="d-flex justify-content-center">
+              <i className="fab fa-cc-paypal fa-3x" />
+            </div>
             <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
               <input type="hidden" name="cmd" value="_s-xclick" />
               <input type="hidden" name="hosted_button_id" value="ER2HYC6HSTJWY" />
 
-              <button type="submit" formtarget="_blank" class="btn btn-primary btn-xl btn-donate">
-                Przekaż darowiznę z
-                <i className="fab fa-cc-paypal fa-3x" />
+              <button type="submit" formtarget="_blank" class="btn btn-primary btn-xl btn-donate m-3">
+                Przekaż darowiznę z PayPal
               </button>
               <img alt="" border="0" src="https://www.paypalobjects.com/pl_PL/i/scr/pixel.gif" />
             </form>
           </div>
-
         </div>
         <div className="col-md-12 col-lg-6">
-          <div class="">
+          <div class="payu-wrap">
             <PayU />
           </div>
         </div>
