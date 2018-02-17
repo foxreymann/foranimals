bc78dfc0ded7ffbb1b1c838c60f200f0ce880a8a
Steve Tyler <mail@steve-tyler.co.uk>
2018-02-17 20:39:53 +0000

    news page styling

--- src/components/News.js
+++ src/components/News.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom'
 import { graphql } from 'react-apollo'
 import gql from 'graphql-tag'
 import catPlaceholder from '../assets/cat.svg'
+import Moment from 'react-moment'
 
 const POSTS_PER_PAGE = 4
 
@@ -18,16 +19,19 @@ const News = ({ data: { loading, error, allPosts, _allPostsMeta }, loadMorePosts
       <section className="content">
         <ul className='News-ul'>
           {allPosts.map(post => (
-            <li className='News-li' key={`post-${post.id}`}>
+            <li className='News-li mb-5' key={`post-${post.id}`}>
               <Link to={`/post/${post.slug}`} className='News-link'>
                 <div className='News-placeholder'>
                   <img
                     alt={post.title}
-                    className='News-img'
-                    src={ post.image ? `https://media.graphcms.com/resize=w:100,h:100,fit:crop/${post.image.handle}` : catPlaceholder }
+                    className='News-img img-fluid'
+                    src={ post.image ? `https://media.graphcms.com/resize=w:600,fit:crop/${post.image.handle}` : catPlaceholder }
                   />
                 </div>
-                <h3>{post.title}</h3>
+                <h5 className="mt-3"><Moment format="DD/MM/YYYY">
+                  {post.date}
+                </Moment></h5>
+                <h4 className="mt-2">{post.title}</h4>
               </Link>
             </li>
           ))}
