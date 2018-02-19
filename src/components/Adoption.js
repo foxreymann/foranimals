78fe13e806d18e048c2c1a5582ce4c1d372cb3d5
Steve Tyler <mail@steve-tyler.co.uk>
2018-02-19 23:06:46 +0000

    added react-modal-image and updated adoptions

--- src/components/Adoption.js
+++ src/components/Adoption.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import Markdown from 'react-markdown'
+import Markdown from 'react-markdown';
+import ModalImage from 'react-modal-image'
 
 class Adoption extends React.Component {
   constructor(props) {
@@ -21,17 +22,21 @@ class Adoption extends React.Component {
       <div>
         <article className="Post">
           <h2>{adoption.name}</h2>
+          <ul className="list-unstyled d-flex flex-wrap">
+            {gallery.map(handle => (
+              <li className="mr-2 mb-2">
+                <ModalImage
+                  small={`https://media.graphcms.com/resize=w:150,fit:crop/${handle}`}
+                  large={`https://media.graphcms.com/resize=w:500,fit:crop/${handle}`}
+                  alt="Hello World!"
+                />
+              </li>
+            ))}
+          </ul>
           <Markdown
             source={adoption.desc}
             escapeHtml={false}
           />
-        <ul className="list-unstyled d-flex flex-wrap">
-            {gallery.map(handle => (
-              <li className="mr-2 mb-2"><img
-                src={`https://media.graphcms.com/resize=w:150,fit:crop/${handle}` }
-              /></li>
-            ))}
-          </ul>
         </article>
       </div>
     );
