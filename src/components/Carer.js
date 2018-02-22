fae9f8332494cc5ce8ee9f24865172c3c862c09e
Steve Tyler <mail@steve-tyler.co.uk>
2018-02-22 20:33:20 +0000

    tidied up CSS and added em elements

--- src/components/Carer.js
+++ src/components/Carer.js
@@ -1,8 +1,8 @@
 import React from 'react'
 
 export default ({carers}) =>
-  <div className="mb-3">
-    <span>Opiekunowie:</span>
+  <div className="mb-3 carers-list">
+    <span><em>Opiekunowie:</em></span>
     {carers.map(carer => (
       <span className="comma" key={carer}> {carer}</span>
     ))}
